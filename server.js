20368b0b4e2b69484dd8ccc1ece3c69639f2ddca
PCCC48 <156000323+PCCC48@users.noreply.github.com>
2024-02-07 13:16:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-/********************************************************************************** 
- * WEB422 – Assignment 1
- * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
- * No part of this assignment has been copied manually or electronically from any other source
- * (including web sites) or distributed to other students.
- * Name: Sakshi Sakshi
- * Student ID: 112602222
- * 
- * Date: 24 January, 2024
- * Cyclic Link: https://thankful-panama-hat-clam.cyclic.app
- * 
- * *********************************************************************************/
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
